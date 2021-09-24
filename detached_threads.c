e383eede6aab856dac78f197d699ec394e69f801
Anuj Rao <anuj.rao@myntra.com>
2021-09-24 08:17:58 +0530

    another way of creating detached threads threads. wrote something about detached vs joinable threads as well. and added an imp. TODO

--- detached_threads.c
+++ detached_threads.c
@@ -15,20 +15,26 @@ void *func() {
 int main(int argc, char *argv[]) {
 
     pthread_t th[2];
+    pthread_attr_t detachedThread;
+    pthread_attr_init(&detachedThread);
+    pthread_attr_setdetachstate(&detachedThread, PTHREAD_CREATE_DETACHED);
+
     for(int i=0;i<2;i++){
-        if (pthread_create(th+i, NULL, &func, NULL) != 0) {
+        if (pthread_create(th+i, &detachedThread, &func, NULL) != 0) {
             printf("failed to create thread\n");
         }
         pthread_detach(th[i]);
     }
 
-    for(int i=0;i<2;i++){
-        if(pthread_join(th[i], NULL)){
-            // detached threads are not joinable. so we will get error at this point
-            printf("failed to join the thread\n");
-        }
-    }
+    // it won't do anything. so let's ignore the below loop
+    // for(int i=0;i<2;i++){
+    //     if(pthread_join(th[i], NULL)){
+    //         // detached threads are not joinable. so we will get error at this point
+    //         printf("failed to join the thread\n");
+    //     }
+    // }
 
+    pthread_attr_destroy(&detachedThread);
     pthread_exit(0);
     return 0;
 }
@@ -39,5 +45,19 @@ int main(int argc, char *argv[]) {
     normal threads clear their resourses when pthread_join is successfully called upon them by main thread.
     ways to detach threads:
         1. create a attached thread and then detach it
-        2. create a deattached thread directly only
+        2. create a deattached thread directly only. this is done by using pthread_attr_t object
+
+    TODO: learn more about the pthread_attr_t attributes which might help in optimizing the
+            threads in some other desired way.
+    
+    Detached vs Joinable threads
+        1. Create a detached thread when you know you won't want to wait for it with pthread_join().
+            The only performance benefit is that when a detached thread terminates, its resources can be
+            released immediately instead of having to wait for the thread to be joined before the resources can be released.
+        2. It is 'legal' not to join a joinable thread; but it is not usually advisable because (as previously noted) 
+            the resources won't be released until the thread is joined, so they'll remain tied up indefinitely (until the 
+            program exits) if you don't join it.
+
+
+
 */
