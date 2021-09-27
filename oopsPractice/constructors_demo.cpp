11f8b44645f75f850873fdd15fb20d6de0e82fd2
Anuj Rao <anuj.rao@myntra.com>
2021-09-27 09:17:51 +0530

    theory about constructors and added a TODO as well

---
+++ oopsPractice/constructors_demo.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+class dataClass {
+    private: 
+        int dataA, dataB;
+    public:
+        dataClass(): dataA(-1), dataB(-2) // this is known as initializer list
+        {
+            cout<<"constructor called"<<endl;
+        }
+        void setData(int d) {
+            dataA = d;
+        }
+        
+        int getData() {
+            return dataA;
+        }
+};
+
+int main(){
+    dataClass d;
+    d.setData(10);
+    cout<<d.getData()<<endl;
+}
+
+/*
+    a little theory about constructors
+        a. constructors should be defined in the "public" block always else 
+            the compiler will give errror
+        b. always use initializer list for initializing the data members. why ?
+            a. members initialized in the initializer list are given a value before
+                the constructor even starts to execute
+            b. initializer list is the only way to initialize const member data and references.
+                TODO: test this thing someday
+*/
